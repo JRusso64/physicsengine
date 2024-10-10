5aadd7068ba22e957c1c945de603037d22ee42c6
JRusso64 <101598072+JRusso64@users.noreply.github.com>
2024-10-09 20:25:58 -0400

    created bouncing ball

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
+var x;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 120);
+  background("aqua");
+  x = 0;
+  speed = 3;
 }
 
 function draw() {
   background("aqua");
+  x = x + speed;
+  if (x > width){
+    speed *= -1;
+  }
+  if (x == 0){
+    speed *= -1;
+  }
+  circle(x, 60, 60, 20, 20);
 }
