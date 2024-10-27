a1b536f7bb27c01f237caa7352de34eba99cbeef
John Russo <jrusso0604@gmail.com>
2024-10-27 00:31:50 -0400

    added functions to shapeinterface

--- sketch/shape.ts
+++ sketch/shape.ts
@@ -3,4 +3,24 @@ import { Vector } from './vector';
 export interface Shape{
   type: number
 
+  draw(): void
+  isInside(point: Vector): boolean
 }
+
+export class AABB implements Shape {
+  size: Vector
+  mass: number
+
+  constructor(size: Vector, mass: number){
+    this.mass = mass;
+    //default size 20 20 if no vector passed in
+    this.size = size || new Vector(20, 20);
+  }
+  
+  function isInside(point: Vector){
+    return False;
+  }
+
+  function draw(){}
+}
+
